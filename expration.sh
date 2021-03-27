2ea5d9c9da3cabc7d010a1b27184062be313d466
sunilyadav <sunil.kumar@safetylabs.org>
2021-03-27 05:49:16 +0000

    commented some file due to faced error in shell script

--- expration.sh
+++ expration.sh
@@ -13,8 +13,8 @@
 	echo $( expr $num1 / $num2 )
 	echo $( expr $num1 % $num2 )
 
-	echo "$num3 + $num4" | bc
-	echo "$num3 - $num4" | bc
-	echo "$num3 * $num4" | bc
-	echo "scale=10;$num3/$num4" | bc
-	echo "$num3 % $num4" | bc
+	#echo "$num3 + $num4" | bc
+	#echo "$num3 - $num4" | bc
+	#echo "$num3 * $num4" | bc
+	#echo "scale=10;$num3/$num4" | bc
+	#echo "$num3 % $num4" | bc
